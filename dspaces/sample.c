f376e565b935cef8f9a4a0060e5a7566ebf15801
lifenggg <lf921227@gmail.com>
2017-03-04 17:17:33 -0500

    add sample.c

---
+++ dspaces/sample.c
@@ -0,0 +1,43 @@
+#include "consumer.h"
+
+// fake one, only select first num_elem_sample
+void prepare_sampled_buffer(float *buffer_region, float *buffer_sample, int num_elems_region, int num_elems_sample){
+    int i;
+    int spacing = region_length*region_length*3;
+    for(i = 0; i< num_elems_sample, i++){
+        memcpy(buffer_region+ spacing, buffer_sample + spacing, spacing*sizeof(float));
+    }
+}
+
+void assign_clusterid(float *buffer_region,int num_region, float * buffer_sample_all, int num_sample_all,float *buffer_medoids,int k,int region_length,int k_npdiv, int div_func,  float *clusterids){
+    int i, j;
+    float clusterid;
+    float divs, divs_tmp;
+    float buffer_a, buffer_b;
+
+    int spacing = region_length*region_length*3;
+
+    for(i = 0; i< num_region; i++){
+        // get divergence to each medoids
+        clusterid = -1;
+        divs = -1;
+        buffer_a = buffer_region + i*spacing;
+
+        for( j = 0; j <k, j++){
+            // compare this region with one medoids
+            buffer_b = buffer_sampled_all + buffer_medoids[j]*spacing;
+            divs_tmp = get_divs( buffer_a , buffer_b, region_length, k_npdiv, div_func);
+            if(div_tmp < divs){
+                divs =div_tmp;
+                clusterid = j;
+            }
+            if(divs_tmp <0){
+                printf("negative divergence !\n");
+                exit(-1);
+            }
+        }
+        clusterids[i] = clusterid;
+    }
+
+}
+
